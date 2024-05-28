749542a4eb954f8588c873d21a79d9c4466d56f8
LROOP2022 <116558329+LROOP2022@users.noreply.github.com>
2024-05-28 13:01:36 +0300

    Обновление данных пользователя

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+from urllib.parse import quote
 import mysql.connector
 
 app = Flask(__name__)
@@ -7,9 +8,10 @@ app = Flask(__name__)
 def query_db(query):
     conn = mysql.connector.connect(
         host='localhost',
-        user='root',
-        password='',
-        database='vkr-v1'
+        user='Daniil',
+        password='(pvXEM1(HAlOqmCm',
+        database='vkr-v1',
+        charset='utf8mb4'
     )
     cursor = conn.cursor()
     cursor.execute(query)
@@ -38,7 +40,8 @@ def lectures():
 def photos():
     query = "SELECT * FROM `periods_photo` WHERE 1"
     periods_photo = query_db(query)
-    return render_template("photos.html", file_names=periods_photo)
+    encoded_file_names = [(file[0], quote(file[1])) for file in periods_photo]
+    return render_template("photos.html", file_names=encoded_file_names)
 
 
 if __name__ == '__main__':
